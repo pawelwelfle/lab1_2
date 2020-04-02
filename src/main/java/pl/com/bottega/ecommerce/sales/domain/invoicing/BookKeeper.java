e65e9e083a685c2f6243d1091c8d746db2870501
pawelwelfle <pawel.welfle@gmail.com>
2020-04-02 14:50:29 +0200

    Changes in Invoice Request

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,10 +21,10 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = Invoice.newInvoice(Id.generate(), client);
+    public Invoice issuance(InvoiceRequest invReq) {
+        Invoice invoice = Invoice.newInvoice(Id.generate(), invReq.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invReq.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
